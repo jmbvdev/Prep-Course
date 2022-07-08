87fdb65549d6528b31b97f78e009e8014cce746c
jmbvdev <jmvb.dev@gmail.com>
2022-07-08 17:34:45 -0400

    finish all js challenges

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -14,12 +14,15 @@ function mayuscula(nombre) {
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb()
+
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1,n2)
 }
 
 function sumarArray(numeros, cb) {
@@ -27,12 +30,20 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  let sum=0
+  for (let i = 0; i < numeros.length; i++) {
+      sum+=numeros[i]     
+  }
+  cb(sum)
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(item=>{
+    cb(item)
+  })
 }
 
 function map(array, cb) {
@@ -40,15 +51,25 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+let arr= array.map(item=>{
+  return cb(item)
+})
+return arr
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
-  array.filter((element,i)=>{
-    return element[i]==="a"
-  })
+  let arr=[]
+  for (let i = 0; i < array.length; i++) {
+   if (array[i][0]==="a") {
+
+      arr.push(array[i])
+   }
+    
+  }
+  return arr
 }
 
 // No modificar nada debajo de esta línea
